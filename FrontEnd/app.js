365d4aad4fc00b874b196b5ed828477cf5f8f6bf
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-07-25 01:23:43 +0530

    POST request

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -34,3 +34,30 @@ function loadCustomer() {
             customerTable.innerHTML = tableRow;
         })
 }
+
+function addCustomer() {
+    let name = document.getElementById("txtName").value;
+    let address = document.getElementById("txtAddress").value;
+    let salary = document.getElementById("txtSalary").value;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "name": name,
+        "address":address,
+        "salary":salary
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://localhost:8080/customer/add", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+} 
